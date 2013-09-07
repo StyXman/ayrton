a8bc8faafd91dbd47e451da9e3e511b65694fa80
Marcos Dione <mdione@grulic.org.ar>
2013-09-07 18:59:09 +0200

    + revamped the globals/locals system. now we hold everything in one class, with several dicts for the different kinds of objects: locals, globals, builtins from the builtins module, builtins added by ayrton and envvars.it definetely breaks messing with locals() and probably globals() too, but works. bigest outcome is that we can pass part of the local environment to the remote, so we can access local variables in the bodies of «with ssh(): ...» statements.

--- ayrton/__init__.py
+++ ayrton/__init__.py
@@ -24,7 +24,7 @@ import importlib
 import builtins
 import ast
 from ast import Pass, Module, Bytes, copy_location, Call, Name, Load
-from ast import fix_missing_locations
+from ast import fix_missing_locations, Import, alias
 import pickle
 
 __version__= '0.1'
@@ -71,27 +71,41 @@ class CommandWrapper (sh.Command):
 
         # mess with the environ
         global runner
-        kwargs['_env']= runner.environ
+        kwargs['_env']= runner.environ.os_environ
 
         return super ().__call__ (*args, **kwargs)
 
-class Globals (dict):
+class Environment (object):
     def __init__ (self):
         super ().__init__ ()
-        # TODO: this is ugly
-        polute (self)
+        self.locals= {}
+        self.globals= {}
+        self.python_builtins= builtins.__dict__.copy ()
+        self.ayrton_builtins= {}
+        polute (self.ayrton_builtins)
+        self.os_environ= os.environ.copy ()
 
     def __getitem__ (self, k):
-        try:
-            ans= getattr (builtins, k)
-        except AttributeError:
+        strikes= 0
+        for d in (self.locals, self.globals, self.os_environ,
+                self.python_builtins, self.ayrton_builtins):
             try:
-                ans= super ().__getitem__ (k)
+                ans= d[k]
             except KeyError:
+                strikes+= 1
 
-                ans= CommandWrapper._create (k)
+        if strikes==5:
+            # the name was not found in any of the dicts
+            # create a command for it
+            ans= CommandWrapper._create (k)
 
         return ans
+
+    def __setitem__ (self, k, v):
+        self.locals[k]= v
+
+    def __iter__ (self):
+        return self.locals.__iter__ ()
 
 class CrazyASTTransformer (ast.NodeTransformer):
 
@@ -104,20 +118,12 @@ class CrazyASTTransformer (ast.NodeTransformer):
             # otherwise we need to create an AST for the call of all the
             # constructors in the body we capture... it's complicated
             m= Module (body=node.body)
+
             data= pickle.dumps (m)
             s= Bytes (s=data)
             s.lineno= node.lineno
             s.col_offset= node.col_offset
             call.args.insert (0, s)
-
-            # add a call to locals() as the second argument to ssh()
-            # this way when we call ssh(), we capture the current environment
-            # and we cans send it and use it as part of the remote environment
-            l= Call(func=Name(id='locals', ctx=Load()), args=[], keywords=[],
-                    starargs=None, kwargs=None)
-            copy_location (l, node)
-            fix_missing_locations (l)
-            call.args.insert (1, l)
 
             p= Pass ()
             p.lineno= node.lineno+1
@@ -136,17 +142,11 @@ class Ayrton (object):
 
         code= ast.parse (script)
         code= CrazyASTTransformer().visit (code)
-
         self.source= compile (code, file, 'exec')
-
-        self.globals= Globals ()
-        self.locals= {}
-
-        # dict to hold the environ used for executed programs
-        self.environ= os.environ.copy ()
+        self.environ= Environment ()
 
     def run (self):
-        exec (self.source, self.globals, self.locals)
+        exec (self.source, self.environ.globals, self.environ)
 
 def polute (d):
     # these functions will be loaded from each module and put in the globals
@@ -176,10 +176,6 @@ def polute (d):
                 dst= function
 
             d[dst]= getattr (m, src)
-
-    # now envvars
-    for k, v in os.environ.items ():
-        d[k]= v
 
     # now the IO files
     for std in ('stdin', 'stdout', 'stderr'):
--- ayrton/functions.py
+++ ayrton/functions.py
@@ -38,16 +38,8 @@ class cd (object):
 
 def export (**kwargs):
     for k, v in kwargs.items ():
-        # NOTE: this is quasi ugly
-        ayrton.runner.globals[k]= str (v)
-        ayrton.runner.environ[k]= str (v)
-
-def unset (*args):
-    for k in args:
-        if k in ayrton.runner.environ.keys ():
-            # found, remove it
-            del ayrton.runner.globals[k]
-            del ayrton.runner.environ[k]
+        ayrton.runner.environ.globals[k]= str (v)
+        ayrton.runner.environ.os_environ[k]= str (v)
 
 def run (path, *args, **kwargs):
     c= ayrton.CommandWrapper._create (path)
@@ -57,9 +49,8 @@ class ssh (object):
     # TODO: inherit CommandWrapper?
     # TODO: see foo.txt
     "Uses the same arguments as paramiko.SSHClient.connect ()"
-    def __init__ (self, code, local_env, *args, **kwargs):
+    def __init__ (self, code, *args, **kwargs):
         self.code= code
-        self.local_env= pickle.dumps (local_env)
         self.args= args
         self.kwargs= kwargs
 
@@ -67,6 +58,8 @@ class ssh (object):
         self.client= paramiko.SSHClient ()
         self.client.load_host_keys (bash ('~/.ssh/known_hosts')[0])
         self.client.connect (*self.args, **self.kwargs)
+        # get the locals from the runtime
+        local_env= pickle.dumps (ayrton.runner.environ.locals)
 
         command= '''python3 -c "import pickle
 from ast import Module, Assign, Name, Store, Call, Load, Expr
@@ -74,11 +67,18 @@ import sys
 c= pickle.loads (sys.stdin.buffer.read (%d))
 code= compile (c, 'remote', 'exec')
 l= pickle.loads (sys.stdin.buffer.read (%d))
-exec (code, {}, l)"''' % (len (self.code), len (self.local_env))
+exec (code, {}, l)"''' % (len (self.code), len (local_env))
         (i, o, e)= self.client.exec_command (command)
         i.write (self.code)
-        i.write (self.local_env)
+        i.write (local_env)
         return (i, o, e)
 
     def __exit__ (self, *args):
         pass
+
+def unset (*args):
+    for k in args:
+        if k in ayrton.runner.environ.globals.keys ():
+            # found, remove it
+            del ayrton.runner.environ.globals[k]
+            del ayrton.runner.environ.os_environ[k]
