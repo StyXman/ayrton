0ec1dd780db5c51d857e5ecb6ced24f50b970645
Marcos Dione <mdione@grulic.org.ar>
2014-12-15 13:02:12 +0100

    [*] pprint() prints itself, more than before :).

--- ayrton/ast_pprinter.py
+++ ayrton/ast_pprinter.py
@@ -1,7 +1,9 @@
 from ast import Module, ImportFrom, Expr, Call, Name, FunctionDef, Assign, Str
 from ast import dump, If, Compare, Eq, For, Attribute, Gt, Num, IsNot, BinOp
-from ast import NameConstant, Mult, Add
+from ast import NameConstant, Mult, Add, Import, List, Dict, Is, BoolOp, And
+from ast import Subscript, Index, Tuple, Lt, Sub
 from _ast import arguments, arg as arg_type, keyword as keyword_type
+from _ast import alias as alias_type
 
 def pprint (node, level=0):
     # move down to the lineno
@@ -18,21 +20,25 @@ def pprint (node, level=0):
 
     elif t==ImportFrom:
         # ImportFrom(module='ayrton.execute', names=[alias(name='Command', asname=None)], level=0)
-        # TODO: level?
-        if len (node.names)>0:
-            # from foo import bar
-            print ("from ", end='')
-            print (node.module, end='')
-            print (" import ", end='')
-            for alias in node.names:
-                print (alias.name, end='')
-                if alias.asname is not None:
-                    print (" as ", end='')
-                    print (alias.asname, end= '')
-                print (', ', end='')
-        else:
-            # import foo
-            print ("import ", node.module, end='')
+        print ("from ", end='')
+        print (node.module, end='')
+        print (" import ", end='')
+        for alias in node.names:
+            pprint (alias)
+            print (', ', end='')
+
+    elif t==Import:
+        # Import(names=[alias(name='ayrton', asname=None)])
+        print ("import ", end='')
+        for alias in node.names:
+            pprint (alias)
+            print (', ', end='')
+
+    elif t==alias_type:
+        print (node.name, end='')
+        if node.asname is not None:
+            print (" as ", end='')
+            print (node.asname, end= '')
 
     elif t==Expr:
         # Expr(value=...)
@@ -105,15 +111,15 @@ def pprint (node, level=0):
 
         if len (node.orelse)>0:
             # special case for elif
-            # if len (node.orelse)==1 and type (node.orelse[0])==If:
-            #     print ('    '*level+'elif ', end='')
-                # TODO
-            # else:
-            print ('    '*level+'else:')
-            for statement in node.orelse:
-                print ('    '*(level+1), end='')
-                pprint (statement, level+1)
-                print ()
+            if len (node.orelse)==1 and type (node.orelse[0])==If:
+                print ('    '*level+'el', end='')
+                pprint (node.orelse[0], level)
+            else:
+                print ('    '*level+'else:')
+                for statement in node.orelse:
+                    print ('    '*(level+1), end='')
+                    pprint (statement, level+1)
+                    print ()
 
     elif t==Compare:
         # Compare(left=Name(id='t', ctx=Load()), ops=[Eq()], comparators=[Name(id='Module', ctx=Load())])
@@ -181,6 +187,58 @@ def pprint (node, level=0):
 
     elif t==Add:
         print ('+', end='')
+
+    elif t==List:
+        print ('[ ', end='')
+        for elt in node.elts:
+            pprint (elt)
+            print (', ', end='')
+        print (' ]', end='')
+
+    elif t==Dict:
+        print ('{', end='')
+        for k, v in zip (node.keys, node.values):
+            pprint (k)
+            print ('=', end='')
+            pprint (v)
+            print (', ', end='')
+        print (' }', end='')
+
+    elif t==Is:
+        print (' is ', end='')
+
+    elif t==BoolOp:
+        for i, v in enumerate (node.values):
+            pprint (v)
+            if i<len (node.values)-1:
+                pprint (node.op)
+
+    elif t==And:
+        print (' and ', end='')
+
+    elif t==Subscript:
+        # Subscript(value=Attribute(value=Name(id='node', ctx=Load()), attr='orelse', ctx=Load()),
+        #           slice=Index(value=Num(n=0)), ctx=Load())
+        pprint (node.value)
+        print ('[', end='')
+        pprint (node.slice)
+        print (']', end='')
+
+    elif t==Index:
+        pprint (node.value)
+
+    elif t==Tuple:
+        print ('( ', end='')
+        for elt in node.elts:
+            pprint (elt)
+            print (', ', end='')
+        print (' )', end='')
+
+    elif t==Lt:
+        print ('<', end='')
+
+    elif t==Sub:
+        print ('-', end='')
 
     else:
         print ()
