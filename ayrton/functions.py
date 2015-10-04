c3c17d332fc3833cc4f6ba26492414fbe1848d02
Marcos Dione <mdione@grulic.org.ar>
2015-10-04 09:59:05 +0200

    [*] big remote() rewrite. [+] use class RemoteStub to encapsulate the fd's returned by paramiko. [+] use a side channel to pass the resulting locals(), the result and a possible Exception  back to the caller. [+] update the globals with the locals from the remote. will need to check if locals is not a better target. [+] raise if there was an exception.

--- ayrton/functions.py
+++ ayrton/functions.py
@@ -27,6 +27,11 @@ import types
 from socket import socket
 from threading import Thread
 import sys
+import subprocess
+import errno
+
+import logging
+logger= logging.getLogger ('ayrton.functions')
 
 # NOTE: all this code is excuted in the script's environment
 
@@ -89,7 +94,22 @@ class CopyThread (Thread):
         self.src.close ()
         self.dst.close ()
 
-class remote (object):
+class RemoteStub:
+    def __init__ (self, i, o, e):
+        self.i= i
+        self.o= o
+        self.e= e
+
+    def close (self):
+        for attr in ('i', 'o', 'e'):
+            f= getattr (self, attr)
+            try:
+                f.close ()
+            except OSError as e:
+                if e.errno!=errno.EBADF:
+                    raise
+
+class remote:
     "Uses the same arguments as paramiko.SSHClient.connect ()"
     def __init__ (self, ast, hostname, *args, **kwargs):
         def param (p, d, v=False):
@@ -107,6 +127,9 @@ class remote (object):
         param ('_python_only', kwargs)
         param ('_debug', kwargs)
         self.kwargs= kwargs
+
+        # socket/transport where the result is going to come back
+        self.result_channel= None
 
     def __enter__ (self):
         # get the globals from the runtime
@@ -133,14 +156,38 @@ code= compile (ast, 'remote', 'exec')
 g= pickle.loads (sys.stdin.buffer.read (%d))
 exec (code, g, {})"''' % (len (self.ast), len (global_env))
         else:
-            command= '''python3 -c "import pickle
-# names needed for unpickling
-from ast import Module, Assign, Name, Store, Call, Load, Expr
-import sys
-import ayrton
-ast= pickle.loads (sys.stdin.buffer.read (%d))
-g= pickle.loads (sys.stdin.buffer.read (%d))
-ayrton.run_tree (ast, g)"''' % (len (self.ast), len (global_env))
+            command= '''python3 -c "import pickle                           #  1
+# names needed for unpickling                                               #  2
+from ast import Module, Assign, Name, Store, Call, Load, Expr               #  3
+import sys                                                                  #  4
+from socket import socket                                                   #  5
+import ayrton                                                               #  6
+                                                                            #  7
+import logging                                                              #  8
+logger= logging.getLogger ('ayrton.remote')                                 #  9
+                                                                            # 10
+ast= pickle.loads (sys.stdin.buffer.read (%d))                              # 11
+g= pickle.loads (sys.stdin.buffer.read (%d))                                # 12
+                                                                            # 13
+logger.debug (ayrton.ast_pprinter.pprint (ast))                             # 14
+logger.debug (g)                                                            # 15
+                                                                            # 16
+runner= ayrton.Ayrton (g)                                                   # 17
+e= None                                                                     # 18
+result= None                                                                # 19
+                                                                            # 20
+try:                                                                        # 21
+    result= runner.run_tree (ast, 'from_remote')                            # 22
+except Exception as e:                                                      # 23
+    pass                                                                    # 24
+                                                                            # 25
+logger.debug (runner.locals)                                                # 26
+                                                                            # 27
+client= socket ()                                                           # 28
+client.connect (('127.0.0.1', 4227))                                        # 29
+client.sendall (pickle.dumps ( (runner.locals, result, e) ))                # 30
+client.close ()                                                             # 31
+"''' % (len (self.ast), len (global_env))
 
         if not self._debug:
             self.client= paramiko.SSHClient ()
@@ -161,14 +208,35 @@ ayrton.run_tree (ast, g)"''' % (len (self.ast), len (global_env))
 
             i.write (command.encode ())
             i.write (b'\n')
+            self.result_channel= socket ()
+            # self.result_channel.setsockopt (SO_REUSEADDR, )
+            self.result_channel.bind (('', 4227))
+            self.result_channel.listen (1)
 
         i.write (self.ast)
         i.write (global_env)
         # TODO: setup threads with sendfile() to fix i,o,e API
-        return (i, o, e)
+        return RemoteStub(i, o, e)
 
     def __exit__ (self, *args):
-        pass
+        (conn, addr)= self.result_channel.accept ()
+        self.result_channel.close ()
+
+        data= b''
+        partial= conn.recv (8196)
+        while len(partial)>0:
+            data+= partial
+            partial= conn.recv (8196)
+
+        (locals, result, e)= pickle.loads (data)
+        logger.debug (locals)
+        conn.close ()
+        ayrton.runner.globals.update (locals)
+        # ayrton.runner.locals.update (locals)
+        logger.debug (ayrton.runner.globals)
+        logger.debug (ayrton.runner.locals)
+        if e is not None:
+            raise e
 
 def run (path, *args, **kwargs):
     c= ayrton.execute.Command (path)
