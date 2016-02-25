9e2500460fb95ef442006bc56bb8f87e12fcc949
Marcos Dione <mdione@grulic.org.ar>
2016-02-25 10:43:02 +0100

    [+] -xxx allows tracing *all* python execution. this is way too verbose (especially because of logging).

--- ayrton/__init__.py
+++ ayrton/__init__.py
@@ -58,6 +58,7 @@ class ExecParams:
         # defaults
         self.trace= False
         self.linenos= False
+        self.trace_all= False
         self.debug= False
 
         self.__dict__.update (kwargs)
@@ -313,6 +314,12 @@ class Ayrton (object):
         if event=='line':
             file_name= frame.f_code.co_filename
             if self.params.trace_all or file_name==self.file_name:
+                l= len (file_name)
+                if l>32:
+                    short_file_name= file_name[l-32:]
+                else:
+                    short_file_name= file_name
+
                 lineno= frame.f_lineno
 
                 line= linecache.getline (file_name, lineno).rstrip ()
@@ -320,7 +327,9 @@ class Ayrton (object):
                     line= self.script[lineno-1].rstrip ()  # line numbers start at 1
 
                 logger.debug2 ('trace e: %s, f: %s, n: %d, l: %s', event, file_name, lineno, line)
-                if self.params.linenos:
+                if self.params.trace_all:
+                    self.trace_line ("+ [%32s:%-6d] %s", short_file_name, lineno, line)
+                elif self.params.linenos:
                     self.trace_line ("+ [%6d] %s", lineno, line)
                 else:
                     self.trace_line ("+ %s", line)
