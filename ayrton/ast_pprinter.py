860413a936f43ce0b3b1d7f45c00bdd446648351
Marcos Dione <mdione@grulic.org.ar>
2016-06-24 23:34:30 +0200

    [+] missing license.

--- ayrton/ast_pprinter.py
+++ ayrton/ast_pprinter.py
@@ -1,3 +1,22 @@
+# -*- coding: utf-8 -*-
+
+# (c) 2013 Marcos Dione <mdione@grulic.org.ar>
+
+# This file is part of ayrton.
+#
+# ayrton is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# ayrton is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with ayrton.  If not, see <http://www.gnu.org/licenses/>.
+
 from ast import Module, ImportFrom, Expr, Call, Name, FunctionDef, Assign, Str
 from ast import dump, If, Compare, Eq, For, Attribute, Gt, Num, IsNot, BinOp
 from ast import NameConstant, Mult, Add, Import, List, Dict, Is, BoolOp, And
