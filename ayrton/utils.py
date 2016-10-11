f04d3f045975ee8b731173ec35cf27ab51441b28
Marcos Dione <mdione@grulic.org.ar>
2016-10-11 13:29:53 +0200

    [*] now we have generic read(), write() and close(), use the original fileobj as is.

--- ayrton/utils.py
+++ ayrton/utils.py
@@ -148,8 +148,8 @@ def copy_loop (copy_to, finished=None, buf_len=10240):
 
         events= selector.select ()
         for key, _ in events:
-            logger.debug (key)
-            i= key.fd
+            logger.debug ("%s is ready to read", key)
+            i= key.fileobj
 
             if finished is not None and i==finished[0]:
                 os.close (finished[0])
