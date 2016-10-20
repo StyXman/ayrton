694ea111577b44955984c9a89b96e0c05a749c60
Marcos Dione <mdione@grulic.org.ar>
2016-10-20 18:46:15 +0200

    [*] factor out a little.

--- ayrton/tests/test_file_tests.py
+++ ayrton/tests/test_file_tests.py
@@ -46,39 +46,79 @@ class TestFalseBool (unittest.TestCase):
 
 class FileTests (unittest.TestCase):
 
-    def test_a_file (self):
-        _, name= tempfile.mkstemp (suffix='.ayrtmp')
-        self.addCleanup (os.unlink, name)
+    def touch_file (self, text=None):
+        fd, name= tempfile.mkstemp (suffix='.ayrtmp')
+        # self.addCleanup (os.unlink, name)
 
-        self.assertTrue (-a (name))
+        if text is not None:
+            os.write (fd, text)
 
-    def test_a_dir (self):
+        os.close (fd)
+
+        return name
+
+
+    def touch_dir (self):
         name= tempfile.mkdtemp (suffix='.ayrtmp')
         self.addCleanup (os.rmdir, name)
 
+        return name
+
+
+    def touch_symlink (self, dir=False):
+        if dir:
+            name= self.touch_dir ()
+        else:
+            name= self.touch_file ()
+
+        # yes, mktemp() is deprecated, but this is a bloody test
+        # and there's no mkltemp()
+        link= tempfile.mktemp (suffix='.ayrtmp')
+        os.symlink (name, link)
+        self.addCleanup (os.unlink, link)
+
+        return link
+
+
+    def test_a_file (self):
+        name= self.touch_file ()
         self.assertTrue (-a (name))
 
     def test_a_symlink (self):
-        name= tempfile.mkdtemp (suffix='.ayrtmp')
-        self.addCleanup (os.unlink, name)
-        name= tempfile.mktemp (suffix='.ayrton')
+        name= self.touch_symlink ()
+        self.assertTrue (-a (name))
 
-        self.assertTrue (-a (link))
+    def test_a_dir (self):
+        name= self.touch_dir ()
+        self.assertTrue (-a (name))
 
-    def test_d (self):
-        name= tempfile.mkdtemp (suffix='.ayrtmp')
-        self.addCleanup (os.rmdir, name)
+    # TODO: -b
 
+    # TODO: -c
+
+
+    def test_d_file (self):
+        name= self.touch_file ()
+        self.assertFalse (-d (name))
+
+    def test_d_symfile (self):
+        name= self.touch_symlink ()
+        self.assertFalse (-d (name))
+
+    def test_d_dir (self):
+        name= self.touch_dir ()
         self.assertTrue (-d (name))
 
-    def test_e (self):
-        _, name= tempfile.mkstemp (suffix='.ayrtmp')
-        self.addCleanup (os.unlink, name)
+    def test_d_symdir (self):
+        name= self.touch_symlink (dir=True)
+        self.assertTrue (-d (name))
 
+
+    def test_e (self):
+        name= self.touch_file ()
         self.assertTrue (-e (name))
 
-    def test_f (self):
-        _, name= tempfile.mkstemp (suffix='.ayrtmp')
-        self.addCleanup (os.unlink, name)
 
+    def test_f_file (self):
+        name= self.touch_file ()
         self.assertTrue (-f (name))
