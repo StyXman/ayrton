df1c783b55727d9ba900ae55f8daa87e98b24486
Marcos Dione <mdione@grulic.org.ar>
2016-11-21 20:36:00 +0100

    [*] version bump (again).

--- ayrton/__init__.py
+++ ayrton/__init__.py
@@ -108,7 +108,7 @@ from ayrton.parser.pyparser.pyparse import CompileInfo, PythonParser
 from ayrton.parser.astcompiler.astbuilder import ast_from_node
 from ayrton.ast_pprinter import pprint
 
-__version__= '0.8.1'
+__version__= '0.8.1.0'
 
 
 class ExecParams:
