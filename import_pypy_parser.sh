099cd6d61f278e0b4bb2cd02647b3928a30aaf66
Marcos Dione <mdione@grulic.org.ar>
2017-02-08 09:12:29 +0100

    [+] script to import pypy's parser into out own tree.

---
+++ import_pypy_parser.sh
@@ -0,0 +1,40 @@
+#! /bin/bash
+
+set -eu
+
+pypy_src="$1"
+
+# NOTE: not every Python version changes the syntax
+for file in astcompiler/__init__.py \
+            astcompiler/astbuilder.py \
+            astcompiler/consts.py \
+            astcompiler/misc.py \
+            astcompiler/tools/Python.asdl \
+            astcompiler/tools/asdl.py \
+            astcompiler/tools/asdl_py.py \
+            astcompiler/tools/spark.py \
+            pyparser/__init__.py \
+            pyparser/automata.py \
+            pyparser/data/Grammar2.5 \
+            pyparser/data/Grammar2.7 \
+            pyparser/data/Grammar3.2 \
+            pyparser/data/Grammar3.3 \
+            pyparser/dfa_generated.py \
+            pyparser/error.py \
+            pyparser/future.py \
+            pyparser/metaparser.py \
+            pyparser/parser.py \
+            pyparser/pygram.py \
+            pyparser/pyparse.py \
+            pyparser/pytoken.py \
+            pyparser/pytokenize.py \
+            pyparser/pytokenizer.py \
+            __init__.py \
+            debug.py \
+            error.py; do
+    src="$pypy_src/pypy/interpreter/$file"
+    dst="ayrton/parser/$file"
+
+    mkdir --parents --verbose "$(dirname $dst)"
+    cp --verbose "$src" "$dst"
+done
