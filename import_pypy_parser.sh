e1eed5c7ef230e3c2340a1f0bf44c72bbdc0debb
Marcos Dione <mdione@grulic.org.ar>
2017-07-18 09:28:29 +0200

    [!] update to py3.6.

--- import_pypy_parser.sh
+++ import_pypy_parser.sh
@@ -2,16 +2,22 @@
 
 set -eu
 
-pypy_src="$1"
+cpython_src="$1"
+pypy_src="$2"
+
+fun copy() {
+    src="$1"
+    dst="$2"
+
+    mkdir --parents --verbose "$(dirname $dst)"
+    cp --verbose "$src" "$dst"
+}
 
 # NOTE: not every Python version changes the syntax
 for file in astcompiler/__init__.py \
             astcompiler/astbuilder.py \
             astcompiler/consts.py \
             astcompiler/misc.py \
-            astcompiler/tools/Python.asdl \
-            astcompiler/tools/asdl.py \
-            astcompiler/tools/asdl_py.py \
             pyparser/__init__.py \
             pyparser/automata.py \
             pyparser/data/Grammar2.5 \
@@ -20,9 +26,9 @@ for file in astcompiler/__init__.py \
             pyparser/data/Grammar3.3 \
             pyparser/data/Grammar3.5 \
             pyparser/data/Grammar3.6 \
-            pyparser/dfa_generated.py \
             pyparser/error.py \
             pyparser/future.py \
+            pyparser/gendfa.py \
             pyparser/metaparser.py \
             pyparser/parser.py \
             pyparser/pygram.py \
@@ -36,6 +42,12 @@ for file in astcompiler/__init__.py \
     src="$pypy_src/pypy/interpreter/$file"
     dst="ayrton/parser/$file"
 
-    mkdir --parents --verbose "$(dirname $dst)"
-    cp --verbose "$src" "$dst"
+    copy("$src", "$dst")
 done
+
+# run generators
+# ayrton/parser/pyparser/dfa_generated.py
+PYTHONPATH=$(pwd) \
+    ./ayrton/parser/pyparser/gendfa.py | \
+    python3.6 -c 'import sys; data = sys.stdin.read(); open(sys.argv[1], "w+").write(data)' \
+    ./ayrton/parser/pyparser/dfa_generated.py
